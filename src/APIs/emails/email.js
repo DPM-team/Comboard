0d9fa544f72bdce7c30d12058e79aebf93d08870
MINAS\User <ics20072@uom.edu.gr>
2022-09-27 21:19:19 +0300

    Write JsDoc for Email class Additions: - JsDoc added for Email class and it's constructor, giving information for its competence, the fields and the returned type.

--- src/APIs/emails/email.js
+++ src/APIs/emails/email.js
@@ -1,5 +1,7 @@
-// This is the class that represents the email to be sent.
-// It has as fields as much data as the sendGrid API needs to know.
+/**
+ * This is the class that represents the email to be sent.
+ * It has as fields as much data as the sendGrid API needs to know.
+ */
 class Email {
   #to;
   #from;
@@ -7,6 +9,14 @@ class Email {
   #text;
   #html;
 
+  /**
+   *
+   * @param {string} to The recipient's email
+   * @param {string} from The sender's email
+   * @param {string} subject The subject of the email to be sent.
+   * @param {string} html The main body of the email, in the form of html code, thus giving possibilities for better content.
+   * @returns {Email} An email instance is returned
+   */
   constructor(to, from, subject, text, html) {
     this.#to = to;
     this.#from = from;
