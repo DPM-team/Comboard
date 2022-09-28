08ba9900075b5b9c2bd33add1c75cbeb0ae67534
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-09-28 20:14:10 +0300

    Implementation of router which runs when a user tries to login Additions: -login router -checkCredentials function

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const { organizationSchema } = require("./organization");
 const { accountSchema } = require("./account");
 const { profileSchema } = require("./profile.js");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   account: {
@@ -15,6 +16,28 @@ const userSchema = mongoose.Schema({
     type: [organizationSchema],
   },
 });
+
+/**
+ * This is a static function that checks the credentials of user.
+ * @param {username} username The username that the user puts in the field.
+ * @param {password} password The password that the user puts in the field.
+ * @returns {user} A user instance is returned.
+ */
+userSchema.statics.checkCredentials = async (username, password) => {
+  const user = await User.findOne({ account: { username } });
+
+  if (!user) {
+    throw new Error("You can not login.Try again");
+  }
+
+  const passwordMatch = bcrypt.compare(password, user.account.password);
+
+  if (!passwordMatch) {
+    throw new Error("You can not login.Try again");
+  }
+
+  return user;
+};
 
 const User = mongoose.model("User", userSchema);
 
---
+++ src/routers/login.js
@@ -0,0 +1,14 @@
+const express = require("");
+const User = require("../models/user");
+const auth = require("../middleware/authentication");
+const router = new express.Router();
+
+//This is the router which runs when one user tries to login.User informaton will be sent back to client.
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = User.checkCredentials(req.body.username, req.body.password);
+    res.send();
+  } catch (e) {
+    res.status(400).send();
+  }
+});
