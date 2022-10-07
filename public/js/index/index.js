ec03e2cba7c1442d011b650e77a4f2adbeb01423
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-10-07 08:25:46 +0300

    CSS and JS added for index page. Additions: - Complete CSS file -> might be refactored - Js UI Controlling Script

---
+++ public/js/index/index.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--features");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".pricing__tab");
+const tabsContainer = document.querySelector(".pricing__tab-container");
+const tabsContent = document.querySelectorAll(".pricing__content");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log("height/width viewport", document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".pricing__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("pricing__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("pricing__content--active"));
+
+  // Activate tab
+  clicked.classList.add("pricing__tab--active");
+
+  // Activate content area
+  document.querySelector(`.pricing__content--${clicked.dataset.tab}`).classList.add("pricing__content--active");
+});
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+// const allSections = document.querySelectorAll(".section");
+
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
