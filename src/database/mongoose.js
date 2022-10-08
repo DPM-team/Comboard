54c85ed57ae6c5fe1667c1e148b24c5dbd1796ef
MINAS\User <ics20072@uom.edu.gr>
2022-10-08 12:37:29 +0300

    Print the error on mongoose connection problem with the mongodb. Additions: - Prints the errors when we got problems with mongodb connection.

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,4 +1,12 @@
 const mongoose = require("mongoose");
 
 //Connect to mongoose with comboard database
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection to handle the error in connection
+const database = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+database.on("error", function () {
+  console.error(console, "MongoDB connection error:");
+});
