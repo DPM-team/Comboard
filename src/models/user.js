760dfa22cc62fcf7202c578fca7f205b1507f185
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-10-25 19:48:33 +0300

    Remove account model and put account fields in user. Corrections: -Put account fields in user model. -Change the method that checks the credits of user.

--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,43 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const { organizationSchema } = require("./organization");
-const { accountSchema } = require("./account");
-const { profileSchema } = require("./profile.js");
 const { Account } = require("./account");
 const { ObjectId } = require("mongodb");
 
 const userSchema = mongoose.Schema({
-  account: {
-    type: mongoose.Schema.Types.ObjectId,
+  username: {
+    type: String,
+    unique: true,
     required: true,
-    ref: "Account",
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    validate: function (value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Wrong email format!");
+      }
+    },
+  },
+  password: {
+    type: String,
+    minlength: 7,
+    trim: true,
+    required: true,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("Please try a different password!");
+      } else if (value.length < 8) {
+        throw new Error("Password must contain at least 8 characters");
+      } else if (!(value.includes("!") || value.includes("@") || value.includes("*"))) {
+        throw new Error("Password must contain at least one special character (!,@,*)");
+      }
+    },
   },
   profile: {
     type: mongoose.Schema.Types.ObjectId,
@@ -24,6 +50,17 @@ const userSchema = mongoose.Schema({
   tokens: {
     type: [],
   },
+});
+
+/**
+ * Middleware function for password security
+ */
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+
+  next();
 });
 
 /**
@@ -50,20 +87,17 @@ userSchema.methods.generateAuthenticationToken = async function () {
  * @returns {user} A user instance is returned.
  */
 userSchema.statics.checkCredentials = async (username, password) => {
-  const userAccount = await Account.findOne({ username });
-  const userPassword = userAccount.password;
+  const user = await User.findOne({ username });
 
-  if (!userAccount) {
+  if (!user) {
     throw new Error("You can not login.Try again");
   }
 
-  const passwordMatch = await bcrypt.compare(password, userPassword);
+  const passwordMatch = await bcrypt.compare(password, user.password);
 
   if (!passwordMatch) {
     throw new Error("You can not login.Try again");
   }
-
-  const user = await User.findOne({ account: userAccount._id });
 
   return user;
 };
--- src/routers/account.js
+++ src/routers/account.js
@@ -4,7 +4,6 @@ const User = require("../models/user.js");
 const Email = require("../APIs/emails/email");
 const sendEmail = require("../APIs/emails/send-email.js");
 const { Profile } = require("../models/profile.js");
-const { Account } = require("../models/account.js");
 
 const router = new express.Router();
 
@@ -14,10 +13,8 @@ router.get("/register", function (req, res) {
 
 router.post("/register/submit", async function (req, res) {
   const profileObj = new Profile({ name: req.body.firstname, surname: req.body.surname });
-  const accountObj = new Account({ username: req.body.username, email: req.body.email, password: req.body.password });
-  const userObj = new User({ account: accountObj._id, profile: profileObj._id });
+  const userObj = new User({ username: req.body.username, email: req.body.email, password: req.body.password, profile: profileObj._id });
   try {
-    await accountObj.save();
     await profileObj.save();
     await userObj.save();
 
