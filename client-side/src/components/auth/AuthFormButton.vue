0344b630de930c736080483af0c7364dc6407704
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-12-29 18:17:37 +0200

    Make auth button. Additions: -auth button

--- client-side/src/components/auth/AuthFormButton.vue
+++ client-side/src/components/auth/AuthFormButton.vue
@@ -1,1 +1,18 @@
-<template></template>
+<template>
+  <button></button>
+</template>
+
+<style scoped>
+button {
+  padding: 0.75rem;
+  display: block;
+  cursor: pointer;
+  background-color: var(--primary-color);
+  color: var(--primary-bright-color);
+  text-align: center;
+  border-radius: 0.25rem;
+  border: 2px solid var(--primary-dark-color);
+  transition-duration: 0.4s;
+  font-weight: bold;
+}
+</style>
