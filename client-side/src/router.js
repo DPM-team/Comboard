de232611e4854344b752f8086af778f89bb0fc5d
MINAS\User <ics20072@uom.edu.gr>
2023-01-04 20:58:54 +0200

    The main configuration for router was creater. Additions: - Install router-vue package. - The router.js file was created, which will hold all the data about he routers in the app. The router object will exported as default.

---
+++ client-side/src/router.js
@@ -0,0 +1,9 @@
+import { createRouter } from "vue-router";
+import { createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: new Array(),
+});
+
+export default router;
