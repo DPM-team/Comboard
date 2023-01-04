3d86234d2a59e246bfbca206e72f1f240dac4b8d
MINAS\User <ics20072@uom.edu.gr>
2023-01-04 15:30:24 +0200

    Index page is constructed Additions: - All components are combined for the index page.

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -1,19 +1,39 @@
 <template>
-  <landing-section></landing-section>
+  <index-page></index-page>
 </template>
 
 <script>
-import LandingSection from "./components/sections/index/LandingSection.vue";
+import IndexPage from "./components/pages/IndexPage.vue";
 
 export default {
   name: "App",
   components: {
-    LandingSection,
+    IndexPage,
   },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+
+:root {
+  --color-primary: #1576bb;
+  --color-secondary: #6dcce9;
+  --color-tertiary: #89e9b7;
+  --color-primary-darker: #0a3c5f;
+  --color-secondary-darker: #41b0d1;
+  --color-tertiary-darker: #37a769;
+  --color-primary-opacity: #73b2e371;
+  --color-demo-opacity: #5be28dbf;
+  --gradient-primary: linear-gradient(to top left, #1b91e5, #bbc3c8);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,8 +42,6 @@ export default {
   color: #2c3e50;
   /* margin-top: 60px; */
 }
-
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
 
 body {
   font-family: "Poppins", sans-serif;
---
+++ client-side/src/components/pages/IndexPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <index-header></index-header>
+    <landing-section></landing-section>
+    <features-section></features-section>
+    <packages-section></packages-section>
+    <testimonials-section></testimonials-section>
+    <index-footer></index-footer>
+  </div>
+</template>
+
+<script>
+import IndexHeader from "../headers/IndexHeader.vue";
+import LandingSection from "../sections/index/LandingSection.vue";
+import FeaturesSection from "../sections/index/FeaturesSection.vue";
+import PackagesSection from "../sections/index/PackagesSection.vue";
+import TestimonialsSection from "../sections/index/TestimonialsSection.vue";
+import IndexFooter from "../footers/IndexFooter.vue";
+
+export default {
+  components: { IndexHeader, LandingSection, FeaturesSection, PackagesSection, TestimonialsSection, IndexFooter },
+};
+</script>
+
+<style>
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  font-weight: 300;
+  color: #444;
+  line-height: 1.9;
+  background-color: #f3f3f3;
+}
+
+p {
+  color: #666;
+}
+
+img {
+  transition: filter 0.5s;
+}
+
+h1 {
+  font-size: 5.5rem;
+  line-height: 1.35;
+}
+
+h4 {
+  font-size: 2rem;
+  font-weight: 500;
+}
+
+.btn:hover {
+  background-color: var(--color-primary-darker);
+}
+
+#text--explanation {
+  padding-top: 10px;
+}
+</style>
