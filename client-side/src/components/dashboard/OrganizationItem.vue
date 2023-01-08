55098bdaef529ba3f885c8161dbc484f27ed8036
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2023-01-08 13:16:35 +0200

    OrganizationItem component added. Additions: - Images - OrganizationItem component

---
+++ client-side/src/components/dashboard/OrganizationItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="column--33perc">
+    <div class="organization">
+      <img :src="formatImagePath" />
+      <div class="title">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    organizationFileName: {
+      type: String,
+      required: true,
+    },
+    organizationLink: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    formatImagePath() {
+      return require("../../assets/images/dashboard/organizations/" + this.organizationFileName);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.column--33perc {
+  flex: 33.33%;
+  max-width: 33.33%;
+  box-sizing: border-box;
+  padding: 15px;
+}
+.organization {
+  background: #fff;
+  border-radius: 8px;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+}
+.organization img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin: 0 auto;
+}
+.title {
+  font-size: 17px;
+}
+</style>
--- client-side/src/components/pages/DashboardPage.vue
+++ client-side/src/components/pages/DashboardPage.vue
@@ -7,6 +7,9 @@
         <dashboard-searchbar></dashboard-searchbar>
         <base-button mode="primary">Join</base-button>
         <base-button mode="secondary">Create</base-button>
+        <organization-item :organizationFileName="'pamak.png'" :title="'Pamak'"></organization-item>
+        <organization-item :organizationFileName="'asoe.jpg'" :title="'Asoe'"></organization-item>
+        <organization-item :organizationFileName="'IEEE.jpg'" :title="'IEEE'"></organization-item>
       </base-card>
     </div>
     <dashboard-footer></dashboard-footer>
@@ -19,13 +22,14 @@ import DashboardHeader from "../layout/headers/DashboardHeader.vue";
 import BaseCard from "../basic-components/BaseCard.vue";
 import DashboardSearchbar from "../dashboard/DashboardSearchbar.vue";
 import BaseButton from "../basic-components/BaseButton.vue";
+import OrganizationItem from "../dashboard/OrganizationItem.vue";
 
 export default {
-  components: { DashboardHeader, DashboardFooter, BaseCard, DashboardSearchbar, BaseButton },
+  components: { DashboardHeader, DashboardFooter, BaseCard, DashboardSearchbar, BaseButton, OrganizationItem },
 };
 </script>
 
-<style scoped>
+<style sc, OrganizationItemoped>
 .content {
   min-height: 700px;
   background: linear-gradient(#ffffff, #1576bb);
