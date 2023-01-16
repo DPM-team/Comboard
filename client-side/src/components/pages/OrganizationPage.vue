89d517453de2eb3019382f2c6e45e7c3f850fb79
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2023-01-16 12:46:12 +0200

    Creation of the organization page and first attempt on the side nav bar Addtions: - Organization page component - Organization side bar nav componennt

---
+++ client-side/src/components/pages/OrganizationPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="organization-page-container"><organization-side-navigation-bar></organization-side-navigation-bar></div>
+</template>
+
+<script>
+import OrganizationSideNavigationBar from "../organization/OrganizationSideNavigationBar.vue";
+export default {
+  components: { OrganizationSideNavigationBar },
+};
+</script>
+
+<style scoped>
+.organization-page-container {
+  min-height: 80vh;
+  /* background: linear-gradient(#ffffff, var(--color-primary));
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto; */
+}
+</style>
