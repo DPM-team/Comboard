e989b58b92217d6aee98e0b3693eeb32fe2cbfa4
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2023-03-04 13:12:10 +0200

    Corrections: - Final postbox component

--- client-side/src/components/organization/network/PostBox.vue
+++ client-side/src/components/organization/network/PostBox.vue
@@ -6,15 +6,14 @@
       <h4>{{ date }}</h4>
     </div>
     <div class="paragraph-container">
-      <p>
-        arxh blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blahBlah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah
-        Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blahBlah blah blah Blah blah blah Blah blah telos
-      </p>
+      <p>{{ postContent }}</p>
     </div>
     <div class="like-comment-container">
-      <p>5 people like this post</p>
-      <font-awesome-icon icon="fa-regular fa-heart" />
-      <font-awesome-icon icon="fa-regular fa-comment" />
+      <p>
+        <b>{{ likeCounter }}</b> people like this post
+      </p>
+      <font-awesome-icon class="post-icon" icon="fa-regular fa-heart" />
+      <font-awesome-icon class="post-icon" icon="fa-regular fa-comment" />
     </div>
   </div>
 </template>
@@ -23,6 +22,9 @@
 export default {
   data() {
     return {
+      postContent:
+        "arxh blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blahBlah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah telos",
+      likeCounter: 12,
       date: "dd/mm/yyyy",
     };
   },
@@ -38,19 +40,26 @@ export default {
   border-radius: 10px;
   width: 600px;
   display: inline-block;
-  height: 250px;
+  /* height: 250px; */
 }
 .image-name-date-container {
+  padding-top: 30px;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.image-name-date-container h2 {
+  width: 350px;
+  font-size: 20px;
 }
 .paragraph-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 150px;
-  overflow-y: auto;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  /* height: 140px;
+  overflow-y: auto; */
   margin: 0 auto;
   width: 80%;
 }
@@ -59,6 +68,16 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-bottom: 30px;
+  margin-top: 5px;
+}
+.like-comment-container p {
+  width: 70%;
+  font-size: 14px;
+}
+.post-icon {
+  margin-right: 12px;
+  font-size: 22px;
 }
 .pfp-container {
   width: 35px;
@@ -66,35 +85,10 @@ export default {
   background-color: var(--color-fourth);
   padding: 0.3em;
   border-radius: 50%;
-  margin-right: 35px;
+  margin-right: 37px;
 }
 .user-pfp {
   width: 100%;
   border-radius: 50%;
-}
-.post-button {
-  background-color: var(--color-primary);
-  color: white;
-  font-size: 12px;
-  border: 1px solid black;
-  display: block;
-  cursor: pointer;
-  text-align: center;
-  border-radius: 15px;
-  transition-duration: 0.4s;
-  /* font-weight: bold; */
-  height: 30px;
-  width: 60px;
-  margin-left: 35px;
-}
-.post-input {
-  width: 350px;
-  padding: 5px 0;
-  font-size: 13px;
-  margin: 10px 0;
-  border: none;
-  border-bottom: 1px solid var(--color-fourth);
-  outline: none;
-  resize: none;
 }
 </style>
