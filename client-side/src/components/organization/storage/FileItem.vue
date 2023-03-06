c3074603cec10689b07d7792a394642a3b5a2b1d
Panagiotis Machairas <ics20044@uom.edu.gr>
2023-03-06 19:07:11 +0200

    Changes in responsive of files. Corrections: -responsive

--- client-side/src/components/organization/storage/FileItem.vue
+++ client-side/src/components/organization/storage/FileItem.vue
@@ -35,7 +35,7 @@ export default {
 
 <style scoped>
 li {
-  width: 6%;
+  width: 8%;
   box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.4);
   box-sizing: border-box;
   border-radius: 15px;
@@ -52,7 +52,7 @@ li div {
   margin-left: auto;
   margin-bottom: 1rem;
   margin-right: auto;
-  width: 50px;
+  width: 80px;
   font-size: 30px;
 }
 
@@ -63,23 +63,23 @@ h6 {
 
 @media screen and (max-width: 1300px) {
   li {
-    width: 8%;
+    width: 10%;
   }
 }
 
 @media screen and (max-width: 1200px) {
   li {
-    width: 10%;
+    width: 14%;
   }
 }
 
 @media screen and (max-width: 1100px) {
   li {
-    width: 12%;
+    width: 16%;
   }
 }
 
-@media screen and (max-width: 900px) {
+@media screen and (max-width: 1000px) {
   li {
     width: 16%;
   }
@@ -88,6 +88,12 @@ h6 {
 @media screen and (max-width: 800px) {
   li {
     width: 20%;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  li {
+    width: 30%;
   }
 }
 
