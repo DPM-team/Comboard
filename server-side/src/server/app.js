1eea4636e9ab46292d67b15e672b7281a8705a59
MINAS\User <ics20072@uom.edu.gr>
2023-03-08 21:10:15 +0200

    Add app.use(express.json());

--- server-side/src/server/app.js
+++ server-side/src/server/app.js
@@ -1,20 +1,19 @@
-const path = require("path");
-const http = require("http");
 const express = require("express");
-const bodyParser = require("body-parser");
 require("../database/mongoose");
+const http = require("http");
+const bodyParser = require("body-parser");
 
 const app = express();
 //We use http module because in socket.io library, we need to pass as argument the HTTP server, but express alone, creates the server behind the scenes and we don't have access to the HTTP server
 const server = http.createServer(app);
 
+app.use(express.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 app.use(bodyParser.json());
-
 //Config the routers
 app.use(require("../routers/connect.js"));
 app.use(require("../routers/logout.js"));
