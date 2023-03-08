2926ece0f2793558fd43d90f2d3c05240545d584
MINAS\User <ics20072@uom.edu.gr>
2023-03-08 19:27:38 +0200

    Style added to ProjectItem component. Additions: - Add css

--- client-side/src/components/organization/projects/ProjectItem.vue
+++ client-side/src/components/organization/projects/ProjectItem.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <h3>{{ projectName }}</h3>
-    <p>{{ projectDescription }}</p>
-  </div>
+  <li>
+    <base-card width="100%">
+      <header>
+        <h3>{{ projectName }}</h3>
+        <nav>
+          <a :href="''">View Project</a>
+        </nav>
+      </header>
+      <p>{{ projectDescription }}</p>
+    </base-card>
+  </li>
 </template>
 
 <script>
@@ -21,7 +28,34 @@ export default {
 </script>
 
 <style scoped>
-div {
-  margin-bottom: 20px;
+li {
+  list-style-type: none;
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
 }
 </style>
