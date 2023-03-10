2dfe6a7fbf106c851fdfb39862a65b88eaf5bb8e
MINAS\User <ics20072@uom.edu.gr>
2023-03-10 19:00:16 +0200

    Customize store for selected organization. Only organization ID is store for now. Additions: - Store for the selected organization

--- client-side/src/store/index.js
+++ client-side/src/store/index.js
@@ -3,11 +3,13 @@ import { createStore } from "vuex";
 // Import all modules
 import authModule from "./modules/auth/index.js";
 import retrieveAccountModule from "./modules/retrieve-account/index.js";
+import organizationModule from "./modules/organization/index.js";
 
 const store = createStore({
   modules: {
     authModule,
     retrieveAccountModule,
+    organizationModule,
   },
 });
 
---
+++ client-side/src/store/modules/organization/mutations.js
@@ -0,0 +1,5 @@
+export default {
+  setSelectedOrganizationID(state, payload) {
+    state.selectedOrganizationID = payload.organizationID;
+  },
+};
