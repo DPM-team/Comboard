18fc17a390bd25e4c5b6eede761243cb8a80b35f
MINAS\User <ics20072@uom.edu.gr>
2023-03-25 15:48:47 +0200

    Get all organization for a specific user based on his id Additions: - getUserOrganizations actione method, to call the api for the data - loadOrganizations method that call the action from the vuex store Corrections: - In the api /api/user/organizations, we get the data from query params instead of the body

--- client-side/src/store/modules/organization/actions.js
+++ client-side/src/store/modules/organization/actions.js
@@ -35,4 +35,41 @@ export default {
       }
     });
   },
+  async getUserOrganizations(_, payload) {
+    const userID = payload.userID;
+
+    try {
+      await fetch(`/api/user/organizations?userID=${userID}`)
+        .then((response) => {
+          // To catch errors from server (e.g. server isn't running)
+          if (!response.ok) {
+            throw new Error("Failed to fetch!");
+          }
+
+          return response.json();
+        })
+        .then((data) => {
+          // To catch errors like user don't found or other error that caused by the server while is on
+          if (data.error) {
+            throw new Error(data.error);
+          }
+
+          const organizations = new Array();
+
+          for (const index in data.organizations) {
+            const organizationObj = {
+              _id: data.organizations[index]._id,
+              name: data.organizations[index].name,
+            };
+
+            organizations.push(organizationObj);
+          }
+
+          console.log(organizations);
+        });
+    } catch (error) {
+      // Fror there, the error will return to the component that calls the action method
+      throw new Error(error.message);
+    }
+  },
 };
