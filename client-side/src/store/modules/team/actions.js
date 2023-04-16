3aeceff7c94337ea88eba7ddcb9b7d966cd497b0
MINAS\User <ics20072@uom.edu.gr>
2023-04-16 21:54:19 +0300

    The functionality to add a team to the organization's team list is completed. Additions: - action that calls the api to add a team to the organization. This action is called from the other action that is called from the ui when the create team form is submitted.

--- client-side/src/store/modules/team/actions.js
+++ client-side/src/store/modules/team/actions.js
@@ -1,16 +1,18 @@
 export default {
-  async createTeam(_, payload) {
+  async createTeam(context, payload) {
     const teamObj = payload.teamObj;
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(teamObj),
+    };
 
     try {
       // Make a POST request to the API endpoint
-      const response = await fetch("/api/team/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(teamObj),
-      });
+      const response = await fetch("/api/team/create", requestOptions);
 
       // Check if the response is successful
       if (!response.ok) {
@@ -22,7 +24,40 @@ export default {
       // Extract the created team data from the response
       const successData = await response.json();
 
+      //Add the created team to the organiation's teams
+      await context.dispatch("addTeamToOrganization", { organizationID: context.rootGetters.selectedOrganizationID, teamID: successData.createdTeam._id });
+
       // Return the created team data
+      return successData;
+    } catch (error) {
+      throw new Error("Failed to create team.");
+    }
+  },
+  async addTeamToOrganization(_, payload) {
+    const organizationID = payload.organizationID;
+    const teamID = payload.teamID;
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ organizationID, teamID }),
+    };
+
+    try {
+      const response = await fetch("/api/organization/team", requestOptions);
+
+      // Check if the response is successful
+      if (!response.ok) {
+        // Handle error response
+        const errorObj = await response.json();
+        throw new Error(errorObj.error);
+      }
+
+      const successData = await response.json();
+
+      // Return the success data from the api call
       return successData;
     } catch (error) {
       console.error(error);
