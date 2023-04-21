25e7b13b675a2fb064fd71cba681cda32a7d4154
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2023-04-21 16:22:02 +0300

    Refactoring and adding the team public profile

---
+++ client-side/src/components/organization/profile/ButtonOptionsItemList.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <div class="item">
+      <h1>{{ text }}</h1>
+      <button v-if="isPrivateProfile">Remove</button><button>View</button>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["text", "isPrivateProfile"],
+};
+</script>
+<style scoped>
+.item {
+  margin-top: 10px;
+}
+.item h1 {
+  font-size: 17px;
+}
+button {
+  margin-left: 3px;
+}
+</style>
