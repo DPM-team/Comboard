15d897efeeadbe0f63d3100ced1079070448571c
MINAS\User <ics20072@uom.edu.gr>
2023-04-24 16:26:26 +0300

    render user's task boards

--- client-side/src/store/modules/tasks/actions.js
+++ client-side/src/store/modules/tasks/actions.js
@@ -28,4 +28,31 @@ export default {
       throw new Error(error.message || "Failed to create Task Board.");
     }
   },
+  async getTaskBoards(context, payload) {
+    const userID = payload.userID;
+    const organizationID = payload.organizationID;
+
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${context.rootGetters.loggedUserToken}`,
+      },
+    };
+
+    try {
+      // Make a POST request to the API endpoint
+      const response = await fetch(`/api/taskboards?userID=${userID}&organizationID=${organizationID}`, requestOptions);
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.error);
+      }
+
+      return responseData?.taskBoards;
+    } catch (error) {
+      throw new Error(error.message || "Failed to get Task Boards.");
+    }
+  },
 };
