28f6727ef238bbed93323019b56d1e1c8672bc2f
MINAS\User <ics20072@uom.edu.gr>
2023-05-20 18:33:17 +0300

    Customize "Today" button in calendar. Update vcalendar package, because the previous version has bugs with slots

--- client-side/src/components/organization/calendar/CalendarTab.vue
+++ client-side/src/components/organization/calendar/CalendarTab.vue
@@ -1,16 +1,17 @@
 <template>
   <organization-page-tab :layout="'block'">
     <div class="calendar-tab-container">
-      <!-- <div class="calendar-container"> -->
       <base-spinner v-if="isLoading"></base-spinner>
       <VCalendar v-else class="calendar" expanded :attributes="attributes" :color="selectedColor" ref="calendar">
+        <template #header-title="title">
+          <h2>{{ title.title }}</h2>
+        </template>
         <template #footer>
-          <div class="">
-            <button class="" @click="moveToday()">Today</button>
+          <div class="btn--container">
+            <button class="today--btn" @click="moveToday()">Today</button>
           </div>
         </template>
       </VCalendar>
-      <!-- </div> -->
     </div>
   </organization-page-tab>
 </template>
@@ -104,13 +105,30 @@ export default {
   align-items: center;
   height: 100%;
 }
+
+.btn--container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 9px;
+}
+
+.today--btn {
+  margin: auto;
+  background-color: var(--color-primary);
+  color: white;
+  width: 10%;
+  height: 45px;
+  font-size: 20px;
+  cursor: pointer;
+}
+
 .calendar-container {
   margin-top: 50px;
   width: 70%;
 }
 
 :deep(.vc-day) {
-  padding: 40px;
+  padding: 50px;
 }
 
 :deep(.vc-day:not(.on-right)) {
@@ -127,41 +145,49 @@ export default {
     width: calc(100% - 180px);
   }
 }
+
 @media (max-width: 1200px) {
   .calendar {
     width: 50%;
   }
 }
+
 @media (max-width: 1150px) {
   .calendar-tab-container {
     width: calc(100% - 80px);
   }
 }
+
 @media (max-width: 900px) {
   :deep(.vc-day) {
     padding: 30px;
   }
 }
+
 @media (max-width: 700px) {
   :deep(.vc-day) {
     padding: 20px;
   }
 }
+
 @media (max-width: 550px) {
   :deep(.vc-day) {
     padding: 10px;
   }
 }
+
 @media (max-width: 400px) {
   :deep(.vc-day) {
     padding: 3px;
   }
 }
+
 @media (max-width: 700px) {
   .calendar-tab-container {
     width: calc(100% - 55px);
   }
 }
+
 @media (max-width: 450px) {
   .calendar-tab-container {
     width: calc(100% - 45px);
