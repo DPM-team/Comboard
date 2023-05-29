a95fb49d9483ebc5acf5c4ef997075dd714aa1b3
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2023-05-29 18:10:03 +0300

    Team public profile is completed and responsive. Additions: - Team image and styling Corrections: - Styling - Responsiveness

--- client-side/src/components/organization/profile/ButtonOptionsItemList.vue
+++ client-side/src/components/organization/profile/ButtonOptionsItemList.vue
@@ -2,7 +2,7 @@
   <li>
     <div class="item">
       <h1>{{ text }}</h1>
-      <button v-if="isPrivateProfile">Remove</button><button>View</button>
+      <button class="remove-btn" v-if="isPrivateProfile">Remove</button><button>View</button>
     </div>
   </li>
 </template>
@@ -22,6 +22,22 @@ export default {
   font-size: 17px;
 }
 button {
+  margin-top: 3px;
+  padding: 0.3rem 0.75rem;
+  font-family: inherit;
+  background-color: var(--color-primary);
+  border: 1px solid var(--color-primary);
+  color: white;
+  cursor: pointer;
+}
+.remove-btn {
   margin-left: 3px;
+  margin-top: 3px;
+  padding: 0.3rem 0.75rem;
+  font-family: inherit;
+  background-color: var(--color-primary);
+  border: 1px solid var(--color-primary);
+  color: white;
+  cursor: pointer;
 }
 </style>
