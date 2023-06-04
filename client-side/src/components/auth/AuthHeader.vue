2bb7fb4ab8bc701706d580ecb614f93a130da5fe
MINAS\User <ics20072@uom.edu.gr>
2023-06-04 19:08:19 +0300

    Add link to comboard logo on auth header

--- client-side/src/components/auth/AuthHeader.vue
+++ client-side/src/components/auth/AuthHeader.vue
@@ -1,9 +1,19 @@
 <template>
   <header>
-    <img src="../../assets/comboard-logo/logo-only-symbol.png" width="70" />
+    <img src="../../assets/comboard-logo/logo-only-symbol.png" width="70" @click.prevent="moveToDashboard()" />
     <slot></slot>
   </header>
 </template>
+
+<script>
+export default {
+  methods: {
+    moveToDashboard() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
 
 <style scoped>
 img {
@@ -11,5 +21,6 @@ img {
   margin-top: -10px;
   margin-left: auto;
   margin-right: auto;
+  cursor: pointer;
 }
 </style>
