5656f0d83016218c0c841dd00f3c40eeb8c2cf23
Panagiotis Machairas <ics20044@uom.edu.gr>
2023-09-26 20:52:07 +0300

    Change server

--- client-side/src/socket.js
+++ client-side/src/socket.js
@@ -8,7 +8,7 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+const URL = process.env.NODE_ENV === "production" ? "https://comboard-service.onrender.com" : "http://localhost:3000";
 
 export const socket = io(URL, {
   transports: ["websocket"],
